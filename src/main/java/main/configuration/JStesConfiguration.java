38f7a773a9052da56d1797448eb1dc0a1edce0f8
Cosmin <tctupangiu@assystem.com>
2017-03-20 14:21:35 +0100

    Simplificare configurare.

--- src/main/java/main/configuration/JStesConfiguration.java
+++ src/main/java/main/configuration/JStesConfiguration.java
@@ -25,6 +25,14 @@ import java.util.List;
  */
 public class JStesConfiguration {
 
+    private final String JOBS_TAG = "jobs";
+    private final String JOB_DEF_TAG ="job_definition";
+    private final String PARAMETERS_TAG="parameters";
+    private final String CODE_TAG = "code";
+    private final String MODULE_TAG = "module";
+    private final String MODULES_TAG = "modules";
+    private final String PYTHON_TAG = "pythonCode";
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
     private static JStesPreferences preferences = new JStesPreferences();
@@ -44,102 +52,98 @@ public class JStesConfiguration {
     public void loadConfiguration(File file) throws IOException {
         XMLWorker xmlWorker = new XMLWorker();
 
+        Document confDocument = null;
         try {
-            Document confDocument = xmlWorker.readFile(file);
-            ParameterSet userConfiguration = new ParameterSet();
-
-            //get user configuration
-            Element user = xmlWorker.getElementByName(confDocument.getDocumentElement(),"user");
-            if (user != null) {
-                for (int i = 0; i < user.getChildNodes().getLength(); i++) {
-                    Node node = user.getChildNodes().item(i);
-                    if (node.getNodeType() == Node.ELEMENT_NODE) {
-                        Element elem = (Element) node;
-                        StringParameter stringParameter = new StringParameter(elem.getTagName(), elem.getTagName(), "User configuration");
-                        stringParameter.setValue(elem.getTextContent());
-                        userConfiguration.addParameter(stringParameter);
-                    }
-                }
-            }
-            preferences.setUserConfiguration(userConfiguration);
-
-            //get the jobs element
-            Element jobs = xmlWorker.getElementByName(confDocument.getDocumentElement(),"jobs");
-            if (jobs != null) {
-                List<Element> jobDefintionElements = xmlWorker.getElementsByName(jobs,"job");
-
-                for (Element element: jobDefintionElements) {
-                    JobDefinition jobDefinition = new JobDefinition();
-                    for (int i = 0; i < element.getChildNodes().getLength(); i++) {
-                        Node node = element.getChildNodes().item(i);
-
-                        if (node.getNodeType() == Node.ELEMENT_NODE) {
-                            Element elem = (Element) node;
-                            if (elem.getTagName().equals("file")) {
-                                readJobDefinitionFile(elem.getTextContent(),jobDefinition);
-                            }
-                            else {
-                                StringParameter stringParameter = new StringParameter(elem.getTagName(), elem.getTagName(), "Job definition");
-                                stringParameter.setValue(elem.getTextContent());
-                                jobDefinition.getParameters().addParameter(stringParameter);
-                            }
-                        }
-
-                    }
-                    preferences.addJobDefition(jobDefinition);
-
-                }
-
-            }
+            confDocument = xmlWorker.readFile(file);
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
             e.printStackTrace();
         }
+
+       ParameterSet userConfiguration = new ParameterSet();
+
+        //get user configuration
+        Element user = xmlWorker.getElementByName(confDocument.getDocumentElement(),"user");
+        if (user != null) {
+            for (int i = 0; i < user.getChildNodes().getLength(); i++) {
+                Node node = user.getChildNodes().item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elem = (Element) node;
+                    StringParameter stringParameter = new StringParameter(elem.getTagName(), elem.getTagName(), "User configuration");
+                    stringParameter.setValue(elem.getTextContent());
+                    userConfiguration.addParameter(stringParameter);
+                }
+            }
+        }
+        preferences.setUserConfiguration(userConfiguration);
+
+
+
+        //get the jobs element
+        Element jobs = xmlWorker.getElementByName(confDocument.getDocumentElement(),JOBS_TAG);
+        if (jobs != null) {
+            List<Element> jobDefinitionElements = xmlWorker.getElementsByName(jobs,JOB_DEF_TAG);
+
+            for (Element element: jobDefinitionElements) {
+                JobDefinition jobDefinition = new JobDefinition();
+
+                try {
+                    readJobDefinitionFile(element.getTextContent(), jobDefinition);
+                    preferences.addJobDefition(jobDefinition);
+                } catch (IllegalArgumentException ex) {
+                    logger.error(ex.getMessage());
+                } catch (ParserConfigurationException e) {
+                    logger.error(e.getMessage());
+                } catch (SAXException e) {
+                    logger.error(e.getMessage());
+                }
+
+            }
+
+        }
+
     }
 
     /**
      * Parse the xml file which define the job and write the parameter set and module names
      * to the {@link JobDefinition}
-     * @param nodeValue
+     * @param filename
      * @param jobDefinition
      */
-    private void readJobDefinitionFile(String nodeValue, JobDefinition jobDefinition) throws IOException, ParserConfigurationException, SAXException,IllegalArgumentException {
+    private void readJobDefinitionFile(String filename, JobDefinition jobDefinition) throws IOException, ParserConfigurationException, SAXException,IllegalArgumentException {
 
-        try {
-            File confFile = new File(JStesConfiguration.class.getClassLoader().getResource(nodeValue).getFile());
+        File confFile = new File(JStesConfiguration.class.getClassLoader().getResource(filename).getFile());
 
-                XMLWorker xmlWorker = new XMLWorker();
-                Document document = xmlWorker.readFile(confFile);
+        XMLWorker xmlWorker = new XMLWorker();
+        Document document = xmlWorker.readFile(confFile);
 
-                Element jobDefinitionElement = document.getDocumentElement();
+        Element jobDefinitionElement = document.getDocumentElement();
 
-                Element parameters = xmlWorker.getElementByName(jobDefinitionElement, "parameters");
-                if (parameters == null) {
-                    logger.error("Parameters element is not found in the job definition element");
-                    throw new IllegalArgumentException("Parameters element is not found in the job definition element");
-                }
-
-                Element code = xmlWorker.getElementByName(jobDefinitionElement, "code");
-                if (code == null) {
-                    logger.error("Code element is not found in the job definition element");
-                    throw new IllegalArgumentException("Code element is not found in the job definition element");
-                }
-
-                Element modules = xmlWorker.getElementByName(jobDefinitionElement, "modules");
-                if (modules == null) {
-                    logger.error("Module element is not found in the job definition element");
-                    throw new IllegalArgumentException("Module element is not found in the job definition element");
-                }
-
-                ParameterSet newSet = createParameters(parameters);
-                newSet.addParameter(createCodeParameter(code));
-                jobDefinition.getParameters().addParameters(newSet);
-                jobDefinition.setModuleManagers(createModuleParameter(modules));
+        Element parameters = xmlWorker.getElementByName(jobDefinitionElement, PARAMETERS_TAG);
+        if (parameters == null) {
+            logger.error("Parameters element is not found in the job definition element");
+            throw new IllegalArgumentException("Parameters element is not found in the job definition element");
         }
-        catch (Exception e) {
-            logger.error(e.getMessage());
+
+        Element code = xmlWorker.getElementByName(jobDefinitionElement, CODE_TAG);
+        if (code == null) {
+            logger.error("Code element is not found in the job definition element");
+            throw new IllegalArgumentException("Code element is not found in the job definition element");
         }
+
+        Element modules = xmlWorker.getElementByName(jobDefinitionElement, MODULES_TAG);
+        if (modules == null) {
+            logger.error("Module element is not found in the job definition element");
+            throw new IllegalArgumentException("Module element is not found in the job definition element");
+        }
+
+        ParameterSet newSet = createParameters(parameters);
+        newSet.addParameter(createCodeParameter(code));
+        jobDefinition.getParameters().addParameters(newSet);
+        jobDefinition.setModuleManagers(createModuleParameter(modules));
+
+
     }
 
     /**
@@ -173,7 +177,7 @@ public class JStesConfiguration {
      */
     private CodeParameter createCodeParameter(Element code) {
 
-        CodeParameter codeParameter = new CodeParameter("pythonCode");
+        CodeParameter codeParameter = new CodeParameter(PYTHON_TAG);
         NodeList nodeList = code.getChildNodes();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node = nodeList.item(i);
@@ -221,7 +225,7 @@ public class JStesConfiguration {
             Node node = nodeList.item(i);
             if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element elem = (Element) node;
-                if (elem.getNodeName().equals("module")) {
+                if (elem.getNodeName().equals(MODULE_TAG)) {
                     moduleSet.add(elem);
                 }
             }
