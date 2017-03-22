ae96a5bda426010e1111df89b224676039ca27c3
Cosmin <tctupangiu@assystem.com>
2017-03-22 13:46:27 +0100

    add injections

--- src/main/java/gui/mainview/sidepanel/info/JobInfoController.java
+++ src/main/java/gui/mainview/sidepanel/info/JobInfoController.java
@@ -1,6 +1,9 @@
 package gui.mainview.sidepanel.info;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,6 +12,19 @@ import java.util.ResourceBundle;
  * Created by tctupangiu on 22/03/2017.
  */
 public class JobInfoController implements Initializable{
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label idLabel;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private TextArea logArea;
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
--- src/main/java/gui/mainview/sidepanel/modules/ModulesController.java
+++ src/main/java/gui/mainview/sidepanel/modules/ModulesController.java
@@ -1,6 +1,9 @@
 package gui.mainview.sidepanel.modules;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,6 +12,22 @@ import java.util.ResourceBundle;
  * Created by tctupangiu on 22/03/2017.
  */
 public class ModulesController implements Initializable{
+
+    @FXML
+    private Button addModuleButton;
+
+    @FXML
+    private Button removeButtonModule;
+
+    @FXML
+    private Button moveupButton;
+
+    @FXML
+    private Button movedownButton;
+
+    @FXML
+    private TableView modulesTable;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
