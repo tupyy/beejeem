f721e1b62c6b956d221f85a850bfd2cd7b009fc3
Cosmin <tctupangiu@assystem.com>
2017-03-22 07:27:40 +0100

    afiseaza jobruile in table. doar asa ca sa pot sa vad ceva

--- src/main/java/gui/mainview/hub/table/JobData.java
+++ src/main/java/gui/mainview/hub/table/JobData.java
@@ -1,51 +1,7 @@
 package gui.mainview.hub.table;
 
+import core.job.Job;
+import core.job.JobState;
+import core.parameters.ParameterSet;
 import javafx.beans.property.SimpleStringProperty;
 
-/**
- * Class which defines the model for the hub table
- */
-public class JobData {
-
-    private final SimpleStringProperty name;
-    private final SimpleStringProperty destinationFolder;
-    private final SimpleStringProperty type;
-    private final SimpleStringProperty aircraft;
-    private final SimpleStringProperty status;
-    private final SimpleStringProperty id;
-
-    public JobData(String name,String id,String destinationFolder,String type,String aircraft,
-                    String status) {
-        this.name = new SimpleStringProperty(name);
-        this.destinationFolder = new SimpleStringProperty(destinationFolder);
-        this.type = new SimpleStringProperty(type);
-        this.aircraft = new SimpleStringProperty(aircraft);
-        this.status = new SimpleStringProperty(status);
-        this.id = new SimpleStringProperty(id);
-    }
-
-
-    public SimpleStringProperty getName() {
-        return name;
-    }
-
-    public SimpleStringProperty getDestinationFolder() {
-        return destinationFolder;
-    }
-
-    public SimpleStringProperty getType() {
-        return type;
-    }
-
-    public SimpleStringProperty getAircraft() {
-        return aircraft;
-    }
-
-    public SimpleStringProperty getStatus() {
-        return status;
-    }
-
-    public SimpleStringProperty getId() {
-        return id;
-    }
-}
