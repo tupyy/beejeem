553e57b73632ac3fac5269a924b4986dad4c692f
Cosmin <tctupangiu@assystem.com>
2017-03-28 16:11:11 +0200

    creare modul preloader

---
+++ jstespreloader/src/preloader/JStesPreloader.java
@@ -0,0 +1,82 @@
+package preloader;
+
+import javafx.application.Preloader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Preloader class for main app. It reads the configuration file and connect to remote host.
+ */
+public class JStesPreloader extends Preloader {
+
+    ProgressBar progressBar;
+    TextArea logArea;
+    VBox pane;
+
+    Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage = primaryStage;
+
+        VBox loading = new VBox(20);
+        loading.setMaxWidth(Region.USE_COMPUTED_SIZE);
+        loading.setMaxHeight(Region.USE_COMPUTED_SIZE);
+
+        logArea = new TextArea();
+        String image = JStesPreloader.class.getClassLoader().getResource("images/logo_splash.png").toExternalForm();
+
+        logArea.getStylesheets().add(JStesPreloader.class.getClassLoader().getResource("textarea.css").toExternalForm());
+        logArea.setEditable(false);
+        logArea.setText("Loading app...");
+        VBox.setVgrow(logArea, Priority.ALWAYS);
+        logArea.setMaxWidth(Region.USE_COMPUTED_SIZE);
+        loading.getChildren().add(logArea);
+
+        progressBar = new ProgressBar();
+        progressBar.setMaxWidth(Region.USE_COMPUTED_SIZE);
+        progressBar.setPrefWidth(584);
+        progressBar.setPrefHeight(20);
+        loading.getChildren().add(progressBar);
+
+
+        BorderPane root = new BorderPane(loading);
+        Scene scene = new Scene(root);
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setWidth(584);
+        primaryStage.setHeight(555);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    @Override
+    public void handleProgressNotification(ProgressNotification pn) {
+
+    }
+
+    @Override
+    public void handleApplicationNotification(PreloaderNotification pn) {
+        if (pn instanceof ProgressNotification) {
+             progressBar.setProgress( ((ProgressNotification) pn).getProgress());
+        }
+        else if(pn instanceof TextNotification) {
+            logArea.setText(logArea.getText() + "\n" + ((TextNotification) pn).getNotification());
+        }
+        else if (pn instanceof StateChangeNotification) {
+            StateChangeNotification sn = (StateChangeNotification) pn;
+            if (sn.getType() == StateChangeNotification.Type.BEFORE_START) {
+                stage.hide();
+            }
+        }
+    }
+    
+}
--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -1,7 +1,15 @@
 package main;
 
+import com.sun.javafx.application.LauncherImpl;
 import core.JStesCore;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.application.Preloader;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,10 +18,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import preloader.JStesPreloader;
+import preloader.TextNotification;
 
 public class MainApp extends Application {
 
     private final JStesCore jStesCore;
+    BooleanProperty ready = new SimpleBooleanProperty(false);
+
 
     public MainApp() {
 
@@ -23,19 +35,68 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        // Do some heavy lifting
+
+
+    }
+
+    private void longStart() {
+        //simulate long init in background
+        Task task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                int max = 10;
+                for (int i = 1; i <= max; i++) {
+                    Thread.sleep(200);
+                    // Send progress to preloader
+                    notifyPreloader(new Preloader.ProgressNotification(((double) i)/max));
+                    Double d = (double) i/max;
+                    notifyPreloader(new TextNotification(String.format("%f",d)));
+                }
+                // After init is ready, the app is ready to be shown
+                // Do this before hiding the preloader stage to prevent the
+                // app from exiting prematurely
+                ready.setValue(Boolean.TRUE);
+
+                notifyPreloader(new Preloader.StateChangeNotification(
+                        Preloader.StateChangeNotification.Type.BEFORE_START));
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // Initiate simulated long startup sequence
+        longStart();
+
+
         VBox rootLayout = null;
 
         Parent root = FXMLLoader.load(MainApp.class.getClassLoader().getResource("mainView.fxml"));
 
         primaryStage.setTitle("JStes");
         primaryStage.setScene(new Scene(root, 1024, 800));
-        primaryStage.show();
+
+        // After the app is ready, show the stage
+        ready.addListener(new ChangeListener<Boolean>(){
+            public void changed(
+                    ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) {
+                if (Boolean.TRUE.equals(t1)) {
+                    Platform.runLater(new Runnable() {
+                        public void run() {
+                            primaryStage.show();
+                        }
+                    });
+                }
+            }
+        });;
+
     }
+
+
 
     @Override
     public void stop(){
@@ -47,6 +108,8 @@ public class MainApp extends Application {
     public static void main(String[] args) {
 
         //launch app
-        Application.launch(MainApp.class,args);
+   //     Application.launch(MainApp.class,args);
+        LauncherImpl.launchApplication(MainApp.class, JStesPreloader.class, args);
+
     }
 }
