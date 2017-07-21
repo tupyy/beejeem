1aa8b67298163e80e086a3c964e5c705b931a51e
cosmin <cosmin@home.net>
2017-07-21 14:56:04 +0200

    delete ModelWorker

--- src/main/java/stes/isami/bjm/ui/hub/logic/HubModelImpl.java
+++ src/main/java/stes/isami/bjm/ui/hub/logic/HubModelImpl.java
@@ -18,25 +18,14 @@ import static stes.isami.bjm.main.JStesCore.getCoreEngine;
 /**
  * Implementation of the {@link HubModel}
  */
-public class HubModelImpl implements HubModel,JobListener,ModelWorker.WorkerActionListener {
+public class HubModelImpl implements HubModel,JobListener {
 
-    public static final int DELETE_STARTED = 0;
-    public static final int DELETE_ENDED = 1;
     private HubController controller;
 
     private ObservableList<JobData> data = FXCollections.observableArrayList();
     private Map<UUID,JobData> data2 = new HashMap<>();
 
-    private ModelWorker modelWorker;
-    private Thread modelWorkderThread;
-
     public HubModelImpl() {
-
-        //start the worker
-        modelWorker = new ModelWorker(this,data,data2);
-        modelWorkderThread= new Thread(modelWorker);
-        modelWorkderThread.start();
-
     }
 
     public void setController(HubController controller) {
@@ -80,15 +69,14 @@ public class HubModelImpl implements HubModel,JobListener,ModelWorker.WorkerActi
                 data2.put(job.getID(),jobData);
                 break;
             case UPDATE:
-                job = getCoreEngine().getJob(event.getId());
-                modelWorker.onUpdateJob(job);
-                break;
             case STATE_CHANGED:
                 job = getCoreEngine().getJob(event.getId());
-                modelWorker.onUpdateJob(job);
+                Platform.runLater(() -> {
+                    final JobData jd = data2.get(event.getId());
+                    jd.updateJob(job);
+                });
                 break;
             case DELETE:
-
                 Platform.runLater(() -> {
                     controller.onStartDelete();
                     for (UUID id: event.getIds()) {
@@ -109,24 +97,6 @@ public class HubModelImpl implements HubModel,JobListener,ModelWorker.WorkerActi
 
     @Override
     public void shutdown() {
-        modelWorkderThread.interrupt();
-    }
-
-    @Override
-    public void onActionStarted(int action) {
-        switch (action) {
-            case DELETE_ACTION:
-                controller.onStartDelete();
-                break;
-            case DELETE_ENDED:
-                controller.onEndDelete();
-                break;
-        }
-    }
-
-    @Override
-    public void onActionEnded(int action) {
-
     }
 
 }
